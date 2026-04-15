6bbcb9e310bb70a94b70ef7597f6acdbb24fb507
devkarore <karore.cg@gmail.com>
2026-04-15 10:17:50 +0200

    TP2: API + filters

--- src/Entity/Director.php
+++ src/Entity/Director.php
@@ -8,32 +8,58 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
+
+
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use Symfony\Component\Serializer\Attribute\Groups;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\ApiResource;
+#[ApiResource(
+    operations : [ 
+        new GetCollection(), 
+        new Get() 
+    ],
+    normalizationContext: ['groups' => ['director:read']]
+)]
+#[ApiFilter(SearchFilter::class, properties: [
+    'nationality' => 'partial'
+])]
+
+
+
 #[ORM\Entity(repositoryClass: DirectorRepository::class)]
 class Director
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['director:read'])]
     private ?int $id = null;
 
     #[Assert\NotBlank(message: "Le prénom est obligatoire")]
     #[Assert\Length(max: 100, maxMessage:"Le prénom ne peut pas faire plus de 100 caractères")]
     #[ORM\Column(length: 100)]
+    #[Groups(['director:read','movie:read'])]
     private ?string $firstName = null;
 
     #[Assert\NotBlank(message: "Le nom est obligatoire")]
     #[Assert\Length(max: 100, maxMessage:"Le nom ne peut pas faire plus de 100 caractères")]
     #[ORM\Column(length: 100)]
+    #[Groups(['director:read','movie:read'])]
     private ?string $lastName = null;
 
     #[Assert\NotBlank(message: "La nationalité est obligatoire")]
     #[ORM\Column(length: 100)]
+    #[Groups(['director:read'])]
     private ?string $nationality = null;
 
     /**
      * @var Collection<int, Movie>
      */
     #[ORM\OneToMany(targetEntity: Movie::class, mappedBy: 'director')]
+    #[Groups(['director:read'])]
     private Collection $movies;
 
     public function __construct()
--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -7,35 +7,63 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
+
+
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use Symfony\Component\Serializer\Attribute\Groups;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\ApiResource;
+#[ApiResource(
+    operations : [ 
+        new GetCollection(), 
+        new Get() 
+    ],
+    normalizationContext: ['groups' => ['movie:read']]
+)]
+#[ApiFilter(SearchFilter::class, properties: [
+    'genre' => 'exact',
+    'director' => 'exact'
+])]
+
+
+
 #[ORM\Entity(repositoryClass: MovieRepository::class)]
 class Movie
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['movie:read'])]
     private ?int $id = null;
 
     #[Assert\NotBlank(message: "Le titre est obligatoire")]
     #[Assert\Length(max: 150, maxMessage: "Le titre ne peut pas faire plus de 150 caractères")]
     #[ORM\Column(length: 150)]
+    #[Groups(['movie:read', 'director:read'])]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['movie:read'])]
     private ?string $synopsis = null;
 
     #[Assert\NotBlank(message: "L'année de sortie est obligatoire")]
     #[Assert\GreaterThan(1888, message: "Petit malin, aucun film n'est aussi vieux :)")]
     #[Assert\LessThanOrEqual(value: 2026, message: "On n'est pas dans le futur !")]
     #[ORM\Column]
+    #[Groups(['movie:read', 'director:read'])]
     private ?int $releaseYear = null;
 
     #[Assert\NotBlank(message: "Le genre est obligatoire")]
     #[ORM\Column(length: 50)]
+    #[Groups(['movie:read'])]
     private ?string $genre = null;
 
     #[Assert\NotNull(message: "Le réalisateur est obligatoire")]
     #[ORM\ManyToOne(inversedBy: 'movies')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['movie:read'])]
     private ?Director $director = null;
 
     public function getId(): ?int
